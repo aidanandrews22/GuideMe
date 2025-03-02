888cbeb915993096d27e8c75b767f60bf1138461
Aidan Andrews <aidansa2@illinois.edu>
2025-03-01 23:26:43 -0600

    Initial Commit

---
+++ GuideMe/GuideMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideMeApp.swift
+//  GuideMe
+//
+//  Created by Aidan Andrews on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GuideMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuideMeTests/GuideMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuideMeTests.swift
+//  GuideMeTests
+//
+//  Created by Aidan Andrews on 3/1/25.
+//
+
+import Testing
+@testable import GuideMe
+
+struct GuideMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
